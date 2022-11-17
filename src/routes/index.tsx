fd6f2a866aef96ed972f344905c9387db1e382a6
Emil Bonne Kristiansen <emilbonnek@gmail.com>
2022-11-17 06:35:41 +0100

    Added some links to /login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { useRouteData } from "solid-start";
+import { A, useRouteData } from "solid-start";
 import {
   createServerAction$,
   createServerData$,
@@ -33,6 +33,20 @@ export default function Home() {
           Logout
         </button>
       </Form>
+      <hr />
+      <a href="/login">&#60;a&#62; link to /login</a>
+      <p>
+        Hitting /login should trigger the redirect back right away. But the
+        redirect doesn't get triggered if the useRouteData ressource isn't being
+        used. This is the same result if I just navigate to /login in the
+        browser.
+      </p>
+      <A href="/login">&#60;A&#62; link to /login</A>
+      <p>
+        This one flashes the /login page for a second but does eventually
+        redirect back. Is there not a way for me to avoid rendering the
+        component in the first place?
+      </p>
     </main>
   );
 }
